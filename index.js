2a6f0cfd27966ef3adf0f6d25c8b09615e41a1aa
Rajesh <rajesh.r@dish.com>
2021-11-06 20:47:42 +0530

    fix: prettier config

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ module.exports = {
         es2021: true,
         node: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 13,
+        ecmaVersion: 2021,
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint'],
+    plugins: ['prettier', '@typescript-eslint'],
     rules: {},
 }
